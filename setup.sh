60fc2df7ec6c085099b208b8f6db37cea65117f2
Sebastian Molenda <sebastian.molenda@pubnub.com>
2023-04-26 12:15:39 +0200

    Added name to .env

--- setup.sh
+++ setup.sh
@@ -10,3 +10,6 @@ echo "PN_PUB_KEY=$pub_key" >> .env
 
 read -p "Enter your Subscribe Key: " sub_key
 echo "PN_SUB_KEY=$sub_key" >> .env
+
+read -p "Enter your Name: " name
+echo "NAME=$name" >> .env
